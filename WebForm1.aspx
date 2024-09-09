ee7345cea78514c185bafaae801efab33a09fb6a
hongnhung2623 <HongNhung.2623@gmail.com>
2024-09-09 19:46:02 +0700

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="web_hinh_tron.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tính Toán Hình Tròn</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Tính Toán Diện Tích và Chu Vi Hình Tròn</h2>
+            <asp:Label ID="lblRadius" runat="server" Text="Nhập bán kính: "></asp:Label>
+            <asp:TextBox ID="txtRadius" runat="server"></asp:TextBox>
+            <asp:Button ID="btnCalculate" runat="server" Text="Tính Toán" OnClick="btnCalculate_Click" />
+            
+            <br /><br />
+            <asp:Label ID="lblResult" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html><%--  --%>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ThuVienHinhTron;
+namespace web_hinh_tron
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double r;
+
+            if (double.TryParse(txtRadius.Text, out r))
+            {
+                if (r > 0)
+                {
+                    try
+                    {
+                        double area = TinhHinhTron.ChuVi(r);
+                        double dt = TinhHinhTron.DienTich(r);
+
+                        lblResult.Text = $"Diện tích hình tròn: {area:F2}<br/>Chu vi hình tròn: {dt:F2}";
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        lblResult.Text = "Lỗi: " + ex.Message;
+                    }
+                }
+                else
+                {
+                    lblResult.Text = "Bán kính phải lớn hơn 0.";
+                }
+            }
+            else
+            {
+                lblResult.Text = "Giá trị nhập vào không hợp lệ. Vui lòng nhập một số thực.";
+            }
+        }
+    }
+}
